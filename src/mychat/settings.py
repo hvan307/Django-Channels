aba7fde8076522c966d76c2028996ae3487bbfd9
hvan307 <hongvan307@gmail.com>
2020-06-05 16:31:11 +0100

    added redis and channel layers working, chat is updated when messages are sent from separate windows

--- src/mychat/settings.py
+++ src/mychat/settings.py
@@ -60,6 +60,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mychat.wsgi.application'
 ASGI_APPLICATION = 'mychat.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
