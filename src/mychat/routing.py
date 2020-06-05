e729a670d48cea82f387426f2e31f91e1d9469bc
hvan307 <hongvan307@gmail.com>
2020-06-05 16:06:03 +0100

    chat messages visible in the chat window, first consumer accepting WS connections works

--- src/mychat/routing.py
+++ src/mychat/routing.py
@@ -1,5 +1,14 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
+import chat.routing
+
+# When connection is made to the Channels development server, the ProtocolTypeRouter will first inspect the type of connection. If it is a websocket connect (ws:// or wss://), the connection will be given to the AuthMiddlewareStack, which the populates the connections scope. Then the connection will be given to the URLRouter
+# Source: https://channels.readthedocs.io/en/latest/tutorial/part_2.html
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
